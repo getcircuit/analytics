585f53cfe6bbefc7fef550cdcf27f1dd9c303199
Christian Kaisermann <christian@kaisermann.me>
2021-03-22 13:26:19 -0300

    refactor: simplify bugsnag plugin

--- src/services/bugsnag/plugin.ts
+++ src/services/bugsnag/plugin.ts
@@ -7,32 +7,15 @@ import type {
 } from '../../types'
 
 type Options = {
-  apiKey?: string
-  client?: Client
+  client: Client
 }
 
 const removeUndefinedProps = (o: unknown) => JSON.parse(JSON.stringify(o))
 
-const bugsnag = ({ apiKey, client }: Options) => {
+const bugsnag = ({ client }: Options) => {
   let bugsnagClient: Client
 
   async function load(this: SharedContext) {
-    if (client == null) {
-      if (apiKey == null) {
-        throw new Error('Needs to pass apiKey or a bugsnag client.')
-      }
-
-      return import('@bugsnag/js').then(({ default: Bugsnag }) => {
-        const { appVersion, env } = this.meta
-
-        client = Bugsnag.start({
-          releaseStage: env,
-          appVersion,
-          apiKey,
-        })
-      })
-    }
-
     bugsnagClient = client
 
     return bugsnagClient
