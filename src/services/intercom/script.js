6962f8002c61b96d3522eaac3caa9f3d6555d076
Christian Kaisermann <christian@kaisermann.me>
2021-06-10 14:05:57 -0300

    fix: intercom script not being loaded

--- src/services/intercom/script.js
+++ src/services/intercom/script.js
@@ -1,33 +1,28 @@
 export function addIntercomScript({ workspaceId }) {
-  ;(function () {
-    var w = window
-    var ic = w.Intercom
-    if (typeof ic === 'function') {
-      ic('reattach_activator')
-      ic('update', w.intercomSettings)
-    } else {
-      var d = document
-      var i = function () {
-        i.c(arguments)
-      }
-      i.q = []
-      i.c = function (args) {
-        i.q.push(args)
-      }
-      w.Intercom = i
-      var l = function () {
-        var s = d.createElement('script')
-        s.type = 'text/javascript'
-        s.async = true
-        s.src = 'https://widget.intercom.io/widget/' + workspaceId
-        var x = d.getElementsByTagName('script')[0]
-        x.parentNode.insertBefore(s, x)
-      }
-      if (w.attachEvent) {
-        w.attachEvent('onload', l)
-      } else {
-        w.addEventListener('load', l, false)
-      }
-    }
-  })()
+  var w = window
+  var ic = w.Intercom
+  if (typeof ic === 'function') {
+    ic('reattach_activator')
+    ic('update', w.intercomSettings)
+    return
+  }
+  var d = document
+  var i = function () {
+    i.c(arguments)
+  }
+  i.q = []
+  i.c = function (args) {
+    i.q.push(args)
+  }
+  w.Intercom = i
+
+  // This script was modified from the original (besides formatting)
+  // Because it uses the onload event of the window to load the script
+  // We don't want that. We want it to load the script when this function is called.
+  var s = d.createElement('script')
+  s.type = 'text/javascript'
+  s.async = true
+  s.src = 'https://widget.intercom.io/widget/' + workspaceId
+  var x = d.getElementsByTagName('script')[0]
+  x.parentNode.insertBefore(s, x)
 }
