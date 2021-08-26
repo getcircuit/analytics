5708675b5bd708eae6ec23d2b380b18efe19217a
Christian Kaisermann <christian@kaisermann.me>
2021-08-26 14:19:37 -0300

    fix: refactor autopilot script tag to prevent null parentNode

--- src/services/autopilot/script.js
+++ src/services/autopilot/script.js
@@ -1,44 +1,34 @@
+const apiBaseURL = 'https://nimblebird.co/anywhere/'
+
 export async function addAutoPilotScript({ apiKey }) {
   if (window.Autopilot) return
-  ;(function (o) {
-    var b = 'https://nimblebird.co/anywhere/',
-      t = apiKey,
-      a = (window.AutopilotAnywhere = {
-        _runQueue: [],
-        run: function () {
-          this._runQueue.push(arguments)
-        },
-      }),
-      c = encodeURIComponent,
-      s = 'SCRIPT',
-      d = document,
-      l = d.getElementsByTagName(s)[0],
-      p =
-        't=' +
-        c(d.title || '') +
-        '&u=' +
-        c(d.location.href || '') +
-        '&r=' +
-        c(d.referrer || ''),
-      j = 'text/javascript',
-      z,
-      y
-    if (!window.Autopilot) window.Autopilot = a
-    if (o.app) p = 'devmode=true&' + p
-    z = function (src, asy) {
-      var e = d.createElement(s)
-      e.src = src
-      e.type = j
-      e.async = asy
-      l.parentNode.insertBefore(e, l)
-    }
-    y = function () {
-      z(b + t + '?' + p, true)
-    }
-    if (window.attachEvent) {
-      window.attachEvent('onload', y)
-    } else {
-      window.addEventListener('load', y, false)
-    }
-  })({ app: true })
+
+  const options = { app: true }
+
+  window.AutopilotAnywhere = {
+    _runQueue: [],
+    run() {
+      this._runQueue.push(arguments)
+    },
+  }
+
+  window.Autopilot = window.AutopilotAnywhere
+
+  let query = [
+    `t=${encodeURIComponent(document.title || '')}`,
+    `u=${encodeURIComponent(document.location.href || '')}`,
+    `r=${encodeURIComponent(document.referrer || '')}`,
+  ].join('&')
+
+  if (options.app) {
+    query = `devmode=true&${query}`
+  }
+
+  const scriptTag = document.createElement('script')
+
+  scriptTag.src = `${apiBaseURL}${apiKey}?${query}`
+  scriptTag.type = 'text/javascript'
+  scriptTag.async = true
+
+  document.head.appendChild(scriptTag)
 }
